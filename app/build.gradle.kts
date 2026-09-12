9b32a1238c48e99dbfe82b68424064b4a7b2a602
redzpector-cloud <redzpector@gmail.com>
2026-09-12 10:37:57 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,15 +15,26 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
